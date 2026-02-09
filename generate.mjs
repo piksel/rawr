72b2541f410f6ad1e19bfa1ff7866894d60f39cb
nils måsén <nils@piksel.se>
2026-02-09 17:53:40 +0100

    fully working, but kinda ugly

--- generate.mjs
+++ generate.mjs
@@ -1,48 +1,84 @@
-import { writeFileSync } from 'node:fs'
+import { writeFileSync, readFileSync } from 'node:fs'
+
+const getRefId = name => {
+    const fc = readFileSync(`${name}.glyph`, {encoding: 'utf-8'});
+    const m = /Encoding: [\d|-]+ [\d|-]+ ([\d|-]+)/gm.exec(fc);
+    if (!m) {
+        throw new Error('Could not find reference ID for ' + name);
+    }
+    console.log('RefID for %o is %o', name, parseInt(m[1], 10));
+    return m[1];
+}
 
 const ranges = [
-    [0x00, 0x19], 
-    0x7F, 
-    [0x80, 0xFF]
+    [0x00, 0x1F], // CONTROL
+    0x7F, // ESC
+    [0x80, 0xFF] // >ASCII
 ];
 const indexes = ranges.flatMap(arg => 
     typeof arg == 'number' ? arg : 
-    Array.from(Array((arg[1] - arg[0]) + 1), (_, i) => i)
+    Array.from(Array((arg[1] - arg[0]) + 1), (_, i) => arg[0] + i)
 )
-// const items = Array.from(ranges, () => (0x7f + i).toString(16).toUpperCase())
 
 const charRefs = {
-    '0': 194 + 0x0,
-    '1': 194 + 0x1,
-    '2': 194 + 0x2,
-    '3': 194 + 0x3,
-    '4': 194 + 0x4,
-    '5': 194 + 0x5,
-    '6': 194 + 0x6,
-    '7': 194 + 0x7,
-    '8': 194 + 0x8,
-    '9': 194 + 0x9,
-    'A': 194 + 0xA,
+    '0': getRefId('zero'),
+    '1': getRefId('one'),
+    '2': getRefId('two'),
+    '3': getRefId('three'),
+    '4': getRefId('four'),
+    '5': getRefId('five'),
+    '6': getRefId('six'),
+    '7': getRefId('seven'),
+    '8': getRefId('eight'),
+    '9': getRefId('nine'),
+    'A': getRefId('_A'), // CAPS = _
+    'B': getRefId('_B'),
+    'C': getRefId('_C'),
+    'D': getRefId('_D'),
+    'E': getRefId('_E'),
+    'F': getRefId('_F'),
 }
+
+const refScale = c => 0.5;//c < 'A' ? 0.5 : 0.7;
+// const refScaleY = 0.5;
+const xPos = 150;
+const yPos = [462, 30];
+
+const boxId = getRefId('rawbox');
+const boxPosX = 0;
+const boxPosY = 0;
+const boxScale = 1;
+const box = '';//`Refer: ${boxId} -1 N ${boxScale} 0 0 ${boxScale} ${boxPosX} ${boxPosY} 0`;
 
 for (let index of indexes) {
     const hexId = index.toString(16).toUpperCase().padStart(2, '0');
     const refId = 211 + index;
+    const glyphFile = `uni00${hexId}.glyph`;
 
-const tpl = `
-StartChar: uni00${hexId}
+    const rendered = `StartChar: uni00${hexId}
 Encoding: ${index} ${index} ${refId}
-Width: 1155
+Width: 600
 Flags: W
 LayerCount: 2
 Fore
-Refer: 204 -1 N 1 0 0 1 0 0 0
-Refer: ${charRefs[hexId[1]]} -1 S 1 0 0 1 637 462 0
-Refer: ${charRefs[hexId[0]]} -1 N 1 0 0 1 637 30 0
+${box}
+Refer: ${charRefs[hexId[0]]} -1 N ${refScale(hexId[0])} 0 0 ${refScale(hexId[0])} ${xPos} ${yPos[0]} 0
+Refer: ${charRefs[hexId[1]]} -1 N ${refScale(hexId[1])} 0 0 ${refScale(hexId[1])} ${xPos} ${yPos[1]} 0
 EndChar
 `;
 
-console.log(tpl);
-break;
-
+    let current = null;
+    try {
+        current = readFileSync(glyphFile, {encoding: 'utf-8'}).replaceAll('\r\n','\n');
+    }
+    catch (e) {console.warn(e)}
+    if (current === rendered) {
+        console.info(`${glyphFile}: Matches!`)
+    } else {
+        console.error(new Error(`${glyphFile}: Differs!`));
+        writeFileSync(glyphFile, rendered, {encoding: 'utf-8'})
+    }
 }
+
+// console.log(tpl);
+// break;
